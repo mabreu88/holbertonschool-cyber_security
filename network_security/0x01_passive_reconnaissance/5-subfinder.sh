fe6e3faf7de9f0d600484d7c88abb5f3d6eb4763
Martin <martymalkav7@gmail.com>
2024-08-10 17:52:21 -0300

    change all syntax

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-subfinder -d $1 -silent | while read d; do h=$(dig +short $d | grep '^[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}$' | head -n 1); echo $d,$h | tee -a $1.txt; done
+subfinder -silent -d $1 -o $1.txt -nW -oI
