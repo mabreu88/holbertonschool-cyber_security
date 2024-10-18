f5247264679a761fb676dc443129e732fef930ff
martin <martymalkav7@gmail.com>
2024-10-18 00:30:13 -0300

    two hours learning how to do an if in bash only to delete it

--- linux_security/0x03_protocols_servers/6-smtp.sh
+++ linux_security/0x03_protocols_servers/6-smtp.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-res=$(grep "^smtpd_tls_security_level" /etc/postfix/main.cf | grep -w "may\|encrypt\|dane")
-if [ -z "$res" ]; then
-    echo "STARTTLS not configured"
-fi
+grep '^smtpd_tls_security_level' /etc/postfix/main.cf
