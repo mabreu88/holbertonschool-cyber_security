497784e4bb0e2455e26c34bd867ef5a62d9a2c5e
martin <martymalkav7@gmail.com>
2024-12-15 18:28:23 -0300

    6. Simple HTTP Request

---
+++ scripting_cyber/0x00-ruby_scripting/6-get.rb
@@ -0,0 +1,20 @@
+require 'net/http'
+require 'json'
+
+def get_request(url)
+    uri = URI(url)
+    response = Net::HTTP.get_response(uri)
+
+    puts "Response status: #{response.code} #{response.message}"
+
+    if response.is_a?(Net::HTTPSuccess)
+        begin
+            json_body = JSON.pretty_generate(JSON.parse(response.body))
+            puts "Response body:\n#{json_body}"
+        rescue JSON::ParserError
+            puts "Response body failure:\n#{json_body}"
+        end
+    else
+        puts "Error:\n#{response.body}"
+    end
+end
